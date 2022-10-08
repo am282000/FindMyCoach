00fa798b5bc5d34b04ae4f37e147dfffae2a2346
Ashish Madan <madaansahaab777@gmail.com>
2022-10-08 20:18:33 +0530

    Find my Coach

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Page Not Found ❌</h2>
+      <h4>
+        This page could not be found - please try again or visit our
+        <router-link to="/coaches">
+          <span class="link">
+            Coaches
+          </span> </router-link
+        >.
+      </h4>
+    </base-card>
+  </section>
+</template>
+<style scoped>
+.link {
+  color: blue;
+}
+.link:hover {
+  color: red !important;
+}
+</style>
